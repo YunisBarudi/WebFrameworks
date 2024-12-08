eb6079477a87a74bb8af246d75f6805635b69964
yunisBeam <yunis@letsbeam.ie>
2024-12-08 15:52:23 +0000

    Update production server URL in fanclubs controller

--- app_server/controllers/fanclubs.js
+++ app_server/controllers/fanclubs.js
@@ -3,7 +3,7 @@ const apiOptions = {
   server: 'http://localhost:3000'
 };
 if (process.env.NODE_ENV === 'production') {
-  apiOptions.server = 'https://your-production-url.com';
+  apiOptions.server = 'https://helloexpressyunisbarudi.onrender.com';
 };
 
 const _renderHomepage = function(req, res, games, clubs) {
